d40022cfefe692d44930b48ac22d1c56e7221dd0
Divya Mahajan <dvm@vanmahajancom>
2016-03-15 02:52:42 -0700

    Updated dashboard:

--- js/app.js
+++ js/app.js
@@ -1,13 +1,8 @@
-var soql = document.getElementById("soql");
-var result = document.getElementById("result");
-var tokentext  = document.getElementById("oauth-token");
+var querydate=moment();
 
-document.getElementById("queryBtn").addEventListener("click", function() {
-    try {
-      tokentext.innerHTML = JSON.stringify(force.oauthStore());
-      console.log("Token:"+force.oauthStore());
-    } catch(e) {
-    }
+document.getElementById("queryBtn").addEventListener("click", function () {
+    var soql = document.getElementById("soql");
+    var result = document.getElementById("result");
     force.query(soql.value,
         function (data) {
             result.innerHTML = JSON.stringify(data, undefined, 3);
@@ -17,4 +12,69 @@ document.getElementById("queryBtn").addEventListener("click", function() {
         });
 }, false);
 
-force.login();
+document.getElementById("refreshBtn").addEventListener("click", afterLogin, false);
+
+function showToken() {
+    try {
+        var tokentext = document.getElementById("oauth-token");
+        tokentext.innerHTML = JSON.stringify(force.oauthStore());
+        console.log("Token:" + force.oauthStore());
+    } catch (e) {
+        console.error(e);
+    }
+}
+function showDate() {
+    try {
+        var datetext = document.getElementById("datestamp");
+        datetext.innerText =  querydate.format("dddd, MMMM Do YYYY, h:mm:ss a"); ;
+    } catch (e) {
+        console.error(e);
+    }
+}
+// Need two named areas
+// <object_name>_summary (h2)
+// <object_name>_changed (h2)
+// <object_name>_table (tbody)
+function getObjectsChanged(object_name,name_field) {
+    var date = querydate;
+    
+    var query = 'select Id,'+name_field+', LastModifiedDate from '+object_name+' where LastModifiedDate > '+date.format()+' limit 10';
+    var soql = document.getElementById("soql");
+    soql.innerText = query;
+    force.query(query, function (response) {
+        var str = '';
+        for (var i = 0; i < response.records.length; i++) {
+            str += '<tr>' +
+            '<td>' + i + '</td>' +
+            '<td>' + response.records[i].Id + '</td>' +
+            '<td>' + response.records[i][name_field] + '</td>' +
+            '<td>' + response.records[i].LastModifiedDate + '</td>' +
+            '</tr>';
+        }
+        document.getElementById(object_name+'_table').innerHTML = str;
+    });
+
+    var changed = document.getElementById(object_name+"_changed");
+    var changed2 = document.getElementById(object_name+"_summary");
+    var query2 = 'select COUNT() from '+object_name+' where LastModifiedDate > '+date.format();
+    force.query(query2, function (response) {
+        try {
+            changed.innerText = response.totalSize +' '+ object_name+ " changes.";
+            changed2.innerText = response.totalSize +' '+ object_name+ " changes.";
+        } catch(e) {
+            console.error(e);
+        }
+    });       
+}
+
+function afterLogin() {
+    querydate=moment().subtract(10,'m');
+    showToken();
+    showDate();
+    getObjectsChanged('Contact','Name');
+    getObjectsChanged('Case','CaseNumber');
+    setTimeout(afterLogin,10000); // Refresh after 1 minute;
+}
+
+force.login(afterLogin);
+
