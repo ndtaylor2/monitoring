3e066e216edf0bf2bf2da41dc4e926d1ca4621c0
Divya van Mahajan <dvm@vanmahajan.com>
2016-03-15 12:21:01 -0700

    Firefox fixed.

--- js/app.js
+++ js/app.js
@@ -13,7 +13,8 @@ document.getElementById("queryBtn").addEventListener("click", function () {
         });
 }, false);
 
-document.getElementById("refreshBtn").addEventListener("click", afterLogin, false);
+document.getElementById("refreshBtn").addEventListener("click", refreshData, false);
+document.getElementById("loginBtn").addEventListener("click", login, false);
 
 function showToken() {
     try {
@@ -27,7 +28,7 @@ function showToken() {
 function showDate() {
     try {
         var datetext = document.getElementById("datestamp");
-        datetext.innerText =  querydate.format("dddd, MMMM Do YYYY, h:mm:ss a"); ;
+        datetext.innerHTML =  querydate.format("dddd, MMMM Do YYYY, h:mm:ss a"); ;
     } catch (e) {
         console.error(e);
     }
@@ -42,7 +43,7 @@ function getObjectsChanged(object_name,name_field) {
     var query = 'select Id,'+name_field+', LastModifiedBy.Name, LastModifiedDate from '+
                 object_name+' where LastModifiedDate > '+date.format()+' ORDER BY LastModifiedDate DESC limit 200';
     var soql = document.getElementById("soql");
-    soql.innerText = query;
+    soql.innerHTML = query;
     force.query(query, function (response) {
         var str = '';
         for (var i = 0; i < response.records.length; i++) {
@@ -62,27 +63,35 @@ function getObjectsChanged(object_name,name_field) {
     var query2 = 'select COUNT() from '+object_name+' where LastModifiedDate > '+date.format();
     force.query(query2, function (response) {
         try {
-            changed.innerText = response.totalSize +' '+ object_name+ " changes.";
-            changed2.innerText = response.totalSize +' '+ object_name+ " changes.";
+            console.log("Count succeeded.");
+            if (changed!=null) {
+                changed.innerHTML = response.totalSize +' '+ object_name+ " changes.";            
+            }
+            if (changed2!=null) {
+                changed2.innerHTML = response.totalSize +' '+ object_name+ " changes.";            
+            }
         } catch(e) {
             console.error(e);
         }
     });       
 }
 
-function afterLogin() {
+function refreshData() {
     querydate=moment().subtract(intervalMinutes,'m');
     showToken();
     showDate();
     getObjectsChanged('Contact','Name');
     getObjectsChanged('Case','CaseNumber');
     getObjectsChanged('ServiceContract','ContractNumber');
-    setTimeout(afterLogin,refreshSeconds*1000); // Refresh after 1 minute;
+    setTimeout(refreshData,refreshSeconds*1000); // Refresh after 1 minute;
 }
-
+function login() {
+    force.login(refreshData);
+}
 if (window.location.hostname=='salty-gorge-66919.herokuapp.com') {
     force.init({appId: '3MVG9KI2HHAq33RzNHP6WHwVK23pR4J56AZuDEZQ9iAIIyAyKjAcom.Lg48fFwojC1YYZ9s00Dw4Ava.3leLi'});
     // This uses a different OAuth app that is setup for salty-gorge-66919.herokuapp.com    
 }
-force.login(afterLogin);
+// login();
+
 
